273b88ec2c57adc70c576fb92b8dfe9d05e08d79
RDX UNLOCK <78124473+rdxunlock@users.noreply.github.com>
2022-08-06 00:32:21 +0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "5507361681:AAHAxPspZygMmjqjFjl137Edzd-gHicZIJY"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1g_Aey_9Ty8R4gujb2HfpWYsCbp0mX8L9"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "1g_Aey_9Ty8R4gujb2HfpWYsCbp0mX8L9"
     
     
     
